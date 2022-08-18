8375070e0147916d08b243a39e9205629d1a7884
Hamza Efe Şahinbaş <85357791+MacallanTheRoot@users.noreply.github.com>
2022-08-18 15:34:52 +0300

    Create veride-tek-cift.py

---
+++ projeler/veride-tek-cift.py
@@ -0,0 +1,13 @@
+#Verilen değere kadar tek çift olan sayıları kontrol eden uygulama Macallantheroot
+
+sayi = int(input("Sayı Giriniz: "))
+    
+for i in range(1,sayi+1):
+    if i % 2 == 0:
+        print(str(i) + " Çiftdir." )
+else:
+    print(str(i) + " Bu Sayı Tekdir.")
+    
+
+
+#Tüm sosyal medya adreslerime https://beacons.ai/macallantheroot 'den ulaşabilirsiniz.
