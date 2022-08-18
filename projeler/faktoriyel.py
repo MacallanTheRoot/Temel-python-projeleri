23820d54d42f4a9266d61512e7ccd9a032fa13c4
Hamza Efe Şahinbaş <85357791+MacallanTheRoot@users.noreply.github.com>
2022-08-18 14:12:03 +0300

    Create faktoriyel.py

---
+++ projeler/faktoriyel.py
@@ -0,0 +1,11 @@
+#Faktoriyel Uygulaması Macallantheroot
+
+def factorial(n): 
+
+    return 1 if (n==1 or n==0) else n * factorial(n - 1);  
+num = int(input("Bir Sayı Giriniz: ")); 
+print("Factorial of",num,"is",
+factorial(num))
+
+
+#Tüm sosyal medya adreslerime https://beacons.ai/macallantheroot 'den ulaşabilirsiniz.
