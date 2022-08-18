35eb6d02090d717abbff3fe8b86bb3e5af47c837
Hamza Efe Şahinbaş <85357791+MacallanTheRoot@users.noreply.github.com>
2022-08-18 13:34:12 +0300

    Create mili-kilometreye-cevirme-uygulamasi.py

---
+++ projeler/mili-kilometreye-cevirme-uygulamasi.py
@@ -0,0 +1,11 @@
+#Mili Kilometreye Çevirme Uygulaması Macallantheroot
+
+mil = float(input("Mil Giriniz: "))
+
+kat_sayı = 1.609344
+
+km = mil * kat_sayı
+print('{0} mil = {1} kilometre. '.format(mil,km))
+
+
+#Tüm sosyal medya adreslerime https://beacons.ai/macallantheroot 'den ulaşabilirsiniz.
